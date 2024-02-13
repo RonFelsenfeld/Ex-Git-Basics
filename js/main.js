ad19c589a7eacbc96743fb4ef4aab176bdba497b
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-02-13 18:14:53 +0200

    Add support for maxDiameter

--- js/main.js
+++ js/main.js
@@ -15,10 +15,10 @@ function onInit() {
   elBall2.style.backgroundColor = getRandomColor()
 }
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
   gSize += getRandomInt(20, 61)
 
-  if (gSize > 400) gSize = 100
+  if (gSize > maxDiameter) gSize = 100
 
   elBall.style.width = gSize + 'px'
   elBall.style.height = gSize + 'px'
